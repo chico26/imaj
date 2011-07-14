40fc0a011f2c4a7941c95b0f551081fd705049c0
Fco. Delgado <chisco-260690@hotmail.com>
2011-07-13 20:15:17 -0500

    Primeras pruebas de chisco

--- application/views/vistaPrueba.php
+++ application/views/vistaPrueba.php
@@ -24,12 +24,36 @@
               'size' => '50 ', 
               'estilo' => 'width: 50%' 
 		); 
+		$options = array(
+				'small'  => 'Small Shirt',
+				'med' => 'Medium Shirt',
+				'large' => 'Large Shirt',
+				'xlarge' => 'Extra Large Shirt'
+				);
+		$shirts_on_sale = array('small', 'large'); 
+		echo form_dropdown('shirts', $options, 'large'); 
+		echo form_dropdown('shirts', $options, $shirts_on_sale);
+		$js = 'id="shirts" onChange="some_function();"'; 
+		echo form_dropdown('shirts', $options, 'large', $js);
+		
+		$string = "</div></div>";
+		echo form_fieldset('Address Information'); 
+		echo "<p>Hola chisco</p>\n";
+		echo form_fieldset_close($string); // Produces <fieldset> <legend>Address Information</legend> <p>form content here</p> </fieldset>
+		
+		echo 'Bicicleta';
+		echo form_checkbox('checkbox','accept',TRUE);
+		echo form_radio('radio','accept',FALSE);
+		
+		echo form_submit('mysubmit', 'Enviar mensaje');
+		
 		foreach($data as $d)
 		{
 			echo form_input($d);
 			echo '<br />';
 		}
 		echo form_password($data); 
+		echo form_upload();
 		echo form_close();
 	?>
 </body>
